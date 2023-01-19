36b488a51127cda6081a01114ca24b55a763d8f9
Nisarg Thoriya <nisarghthoroya@gmail.com>
2023-01-19 14:43:41 +0530

    Update user-data.sh

--- tf-files/user-data.sh
+++ tf-files/user-data.sh
@@ -7,5 +7,5 @@ pip3 install flask
 pip3 install flask_mysql
 yum install git -y
 TOKEN="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"
-cd /home/ec2-user && git clone https://$TOKEN@github.com/devenes/terraform-flask-app.git
+cd /home/ec2-user && git clone https://$TOKEN@github.com/nisarg14/terraform-flask-app.git
 python3 /home/ec2-user/terraform-flask-app/phonebook-app.py
