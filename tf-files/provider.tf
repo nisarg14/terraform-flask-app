7e423b00b8459faab3a90aad7373bc036db98e3d
Nisarg Thoriya <nisarghthoroya@gmail.com>
2023-01-19 14:44:48 +0530

    Update provider.tf

--- tf-files/provider.tf
+++ tf-files/provider.tf
@@ -1,24 +1,24 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "4.10.0"
-    }
-    github = {
-      source  = "integrations/github"
-      version = "4.23.0"
-    }
-  }
-  backend "s3" {
-    bucket = "terraform-env-bucket"
-    key    = "key/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "4.10.0"
+#     }
+#     github = {
+#       source  = "integrations/github"
+#       version = "4.23.0"
+#     }
+#   }
+#   backend "s3" {
+#     bucket = "terraform-env-bucket"
+#     key    = "key/terraform.tfstate"
+#     region = "us-east-1"
+#   }
+# }
 
 provider "aws" {
   region  = var.region
-  profile = var.profile
+#   profile = var.profile
 }
 
 provider "github" {
